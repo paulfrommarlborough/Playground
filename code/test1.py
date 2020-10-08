a021c9fb3169669bbe7c6bd677a55b244e12e7c1
Paul Douglas <paulfrommarlborough@gmail.com>
2020-10-08 12:16:48 -0400

    test checkin

---
+++ code/test1.py
@@ -0,0 +1,15 @@
+print('ODD')
+odd_numbers = list(range(1, 100, 2))
+print (odd_numbers)
+
+print("SQUARES")
+squares = [ value **2 for value in range(1,100)]
+print (squares)
+
+print("CUBES")
+cubes = [ value ** 3 for value in range (1, 100)]
+#for i in range(1,10):
+#    print(cubes[i])
+#cubes.append(12)
+cubes.sort(reverse=True)
+print (cubes[1:10])
