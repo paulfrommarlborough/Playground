1867ea84df4bb0d8cb437f0baeccabc8a2774dbb
Paul Douglas <paulfrommarlborough@gmail.com>
2020-10-19 21:41:20 -0400

    remove unused code.

--- code/rename_with_creationtime.py
+++ code/rename_with_creationtime.py
@@ -1,5 +1,5 @@
 # mv1.py:   get a list of files from a directory
-# rename with date of creation.
+# rename with date of creation. 
 #----------------------------------------------------------
 import os
 import time
@@ -7,23 +7,19 @@ from os import listdir
 from os.path import  isfile,join
 from datetime import datetime
 
-#now = datetime.now()
-#current_dm = now.strftime("20%y%m%d")
-
-mypath = 'C:\\paul\\tmp'
+mypath = 'C:\\paul\\temp'
 ofiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 filecount = 1
 
 for cfile in ofiles:
     fp_cfile1 = f"{mypath}\\{cfile}"
     created = os.path.getmtime(fp_cfile1)
-    current_dm = datetime.fromtimestamp(created).strftime("20%y%m%d")    
-    fx = f'_xh_{0}.jpg'.format(current_dm)
+    current_dm = datetime.fromtimestamp(created).strftime("20%y%m%d") 
     newfile = f"{filecount:05d}_xh_{current_dm}.jpg"
     print(f"File: {cfile}  Date created: {time.ctime(created)}")
-    msg = f"Rename:({cfile},{newfile})"
     filecount = filecount + 1   
     fp_newfile = f"{mypath}\\{newfile}" 
-    os.rename(fp_cfile1, fp_newfile)
+    msg = f"Rename:({cfile},{newfile})"
     print(msg)
+    os.rename(fp_cfile1, fp_newfile)
 
