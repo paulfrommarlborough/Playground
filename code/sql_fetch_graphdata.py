ede02e225e57b08163385ef95e872ca602c5a142
paul douglas <paulfrommarlborough@gmail.com>
2020-11-02 21:31:45 -0500

    testing matplotlib

---
+++ code/sql_fetch_graphdata.py
@@ -0,0 +1,57 @@
+""" pyodbc  test
+       pip install pyodbc
+       cd python\scripts
+       pip install pyodbc  
+"""
+
+import pyodbc
+import csv
+
+
+print ("try pyodbc...")
+database_connect_properties = ['{ODBC Driver 17 for SQL Server}', 'tcp:192.168.5.153,1433', 'pawz', 'PAWZDSN', 'LuapJarusEiddam+1']
+connect_str =  'DRIVER=' + database_connect_properties[0] + ';SERVER=' + database_connect_properties[1] + ';DATABASE=' + database_connect_properties[2]+ ';UID=' + database_connect_properties[3]+';PWD='+database_connect_properties[4] 
+
+print (database_connect_properties)
+print(connect_str)
+
+conn = pyodbc.connect(connect_str) 
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM nodepolicy")
+rs = cursor.fetchall()
+
+for row in rs:
+    print (f'row  = {row[1]}')
+    print(f"row = {row}")
+
+#with open ("new.csv", "w") as file:
+#    for row in rs:
+#        csv.writer(file).writerow(row)
+
+cursor.close()
+del cursor
+
+
+cursor1 = conn.cursor()
+cursor1.execute("SELECT max(timeid) from time")
+rs = cursor1.fetchall()
+for row in rs:
+    print (f'maxtimeid  = {row}')
+    timeid =int(row[0])
+    print('timeid as int = {timeid}')
+    cursor2 = conn.cursor()
+    sql = f'select * from graphdata where timeid = {timeid} and graphid=110'
+    cursor2.execute(sql)
+    rs2=cursor2.fetchall()
+    for r in rs2:
+        graphdata = r[8]
+        print(f'graphdata { graphdata }')
+
+    cursor2.close()
+    del (cursor2)
+
+cursor1.close()
+del (cursor1)
+conn.close()
+
+print ("Done.")
