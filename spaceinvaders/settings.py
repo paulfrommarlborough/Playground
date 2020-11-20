4f4d6c3d65d446bf27ffa77ab2661d9c7c4b995d
paul douglas <paulfrommarlborough@gmail.com>
2020-11-19 21:40:33 -0500

    based on python crashcourse

--- spaceinvaders/settings.py
+++ spaceinvaders/settings.py
@@ -2,7 +2,7 @@ class Settings:
     """ class to store settings """
     def __init__(self):
         """ initilaize settings """
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 800
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         
