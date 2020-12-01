1346d4242ebe8fa9f4c2ddd44927a9b2fd3d1e4a
paul douglas <paulfrommarlborough@gmail.com>
2020-11-30 22:12:14 -0500

    add bullet.

--- spaceinvaders/settings.py
+++ spaceinvaders/settings.py
@@ -5,4 +5,9 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        #bullet settings
+        self.bullet_speed=1
+        self.bullet_width=3
+        self.bullet_height = 15
+        self.bullet_color= (60,60,60)
         
--- spaceinvaders/spaceinvaders_main.py
+++ spaceinvaders/spaceinvaders_main.py
@@ -2,24 +2,30 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """ spaceinvaders_main.py  stub """
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
+
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
 
     def event_key_down(self,event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -43,8 +49,14 @@ class AlienInvasion:
     def update_screen(self):
             # Redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
-            
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -53,6 +65,13 @@ class AlienInvasion:
             self.check_events()
 
             self.ship.update()
+            
+            self.bullets.update()
+
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
+                    print("remove")
 
             self.update_screen()
 
