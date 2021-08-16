d5b967781a26ff6d7ebccab47ae53c937544507e
Paul Douglas <paulfrommarlborough@gmail.com>
2021-08-16 12:28:04 -0400

    re-org

--- Code/RenameFiles/rename_with_creationtime.py
+++ Code/RenameFiles/rename_with_creationtime.py
@@ -7,6 +7,8 @@ from os import listdir
 from os.path import  isfile,join
 from datetime import datetime
 
+
+
 mypath = 'C:\\paul\\temp'
 ofiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 filecount = 10
@@ -15,7 +17,7 @@ for cfile in ofiles:
     fp_cfile1 = f"{mypath}\\{cfile}"
     created = os.path.getmtime(fp_cfile1)
     current_dm = datetime.fromtimestamp(created).strftime("20%y%m%d") 
-    newfile = f"xh_{filecount:05d}_{current_dm}.jpg"
+    newfile = f"xh1_{filecount:03d}_{current_dm}.jpg"
     print(f"File: {cfile}  Date created: {time.ctime(created)}")
     filecount = filecount + 1   
     fp_newfile = f"{mypath}\\{newfile}" 
