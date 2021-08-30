793e474901f1cce72c45b2b2adb4d048a77554be
Paul Douglas <paulfrommarlborough@gmail.com>
2021-08-29 20:47:12 -0400

    add -yesterday

--- CouchCode/CouchAdd/run_nightly.bat
+++ CouchCode/CouchAdd/run_nightly.bat
@@ -1,1 +1,1 @@
-python CouchAddMain.py --input C:\tools\couch\nightly\input.json --workdir C:\tools\couch\nightly --username admin --password pawz1 --server 192.168.5.158:5984
+python CouchAddMain.py --input C:\tools\couch\nightly\input.json --workdir C:\tools\couch\nightly --username admin --password pawz1 --server 192.168.5.158:5984 --yesterday
