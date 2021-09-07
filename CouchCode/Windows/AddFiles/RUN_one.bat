ab80aaec808e39737e94c7230356735836630ee6
Paul Douglas <paulfrommarlborough@gmail.com>
2021-09-06 20:50:56 -0400

    add root variablbe

--- CouchCode/Windows/AddFiles/RUN_one.bat
+++ CouchCode/Windows/AddFiles/RUN_one.bat
@@ -1,1 +1,2 @@
-python CouchAddMain.py --yesterday --host PALLADIUM   --os WINDOWS --ip 192.168.5.158 --workdir C:\tools\couch\nightly\tmp --username admin --password pawz1 --server 192.168.5.158:5984
+set root="C:\tools\CouchCode\Windows\AddFiles"
+python CouchAddMain.py --yesterday --host PALLADIUM   --os WINDOWS --ip 192.168.5.158 --workdir %root% --username admin --password pawz1 --server 192.168.5.158:5984
--- CouchCode/Windows/AddFiles/run_addall.bat
+++ CouchCode/Windows/AddFiles/run_addall.bat
@@ -1,1 +1,2 @@
-python CouchAddMain.py --all --input C:\tools\couch\nightly\input.json --workdir C:\tools\couch\nightly --username admin --password pawz1 --server 192.168.5.158:5984
+set root="C:\tools\CouchCode\Windows\AddFiles"
+python CouchAddMain.py --all --input %root%\\input.json --workdir %root --username admin --password pawz1 --server 192.168.5.158:5984
